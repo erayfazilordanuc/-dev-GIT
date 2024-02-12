0506db862759f679aea35c30b36351ffcce21de7
erayfazilordanuc <fazilordanuc@gmail.com>
2024-02-12 22:17:52 +0300

    grade

--- Not Ortalaması/gradeaverage.java
+++ Not Ortalaması/gradeaverage.java
@@ -1,61 +1,48 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class gradeaverage{
-    public static void main(String[] args){
+public class notortalama {
+    public static void main(String[] n){
     
     Scanner input = new Scanner(System.in);
 
-    System.out.println("Enter your Math score");
-     int math=input.nextInt();
+    System.out.println("Enter your math score");
+    int math = input.nextInt();
     if(math<0||math>100){
-        System.out.println("Out of range! Please enter a number between 100-0");
-     math=input.nextInt();
+        System.out.println("Please enter a score between 0-100");
+        math = input.nextInt();
     }
-
-    System.out.println("Enter your Physics score");
-     int physics=input.nextInt();
+    System.out.println("Enter your physics score");
+    int physics = input.nextInt();
     if(physics<0||physics>100){
-        System.out.println("Out of range! Please enter a number between 100-0");
-     physics=input.nextInt();
+        System.out.println("Please enter a score between 0-100");
+        physics = input.nextInt();
     }
-     
-    System.out.println("Enter your Chemics score");
-     int chemics=input.nextInt();
-    if(chemics<0||chemics>100){
-        System.out.println("Out of range! Please enter a number between 100-0");
-     chemics=input.nextInt();
-    }
-
-    System.out.println("Enter your Turkish score");
-     int turkish=input.nextInt();
+    System.out.println("Enter your turkish score");
+    int turkish = input.nextInt();
     if(turkish<0||turkish>100){
-        System.out.println("Out of range! Please enter a number between 100-0");
-     turkish=input.nextInt();
+        System.out.println("Please enter a score between 0-100");
+        turkish = input.nextInt();
     }
-
-    System.out.println("Enter your History score");
-     int history=input.nextInt();
-    if(history<0||history>100){
-        System.out.println("Out of range! Please enter a number between 100-0");
-     history=input.nextInt();
+    System.out.println("Enter your chemical score");
+    int chemical = input.nextInt();
+    if(chemical<0||chemical>100){
+        System.out.println("Please enter a score between 0-100");
+        chemical = input.nextInt();
     }
-
-    System.out.println("Enter your Music score");
-     int music=input.nextInt();
+    System.out.println("Enter your music score");
+    int music = input.nextInt();
     if(music<0||music>100){
-        System.out.println("Out of range! Please enter a number between 100-0");
-     music=input.nextInt();
+        System.out.println("Please enter a score between 0-100");
+        music = input.nextInt();
     }
-    
-    double ratio=(math+music+history+turkish+physics+chemics)/6;
 
-    System.out.println(ratio);
+    double ratio = (math+music+turkish+chemical+physics)/5;
 
-    if(ratio<60){
-        System.out.println("Can't passed");
+    System.out.println("Your average is "+ratio);
+
+    if(ratio>=55){
+        System.out.println("You passed");
+    }else{
+        System.out.println("You can't passed");
     }
-    if(ratio>60){
-        System.out.println("Passed");
-    }
-    }
-}
+}}
